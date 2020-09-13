7bb48b3709af7e8aac24ea5a214d03272db45085
luke-h1 <luke.howsam@yahoo.com>
2020-09-13 20:21:34 +0100

    init commit

---
+++ NOTES.md
@@ -0,0 +1,72 @@
+## APi:
+
+
+BASE_URL = https://randomuser.me/api/?results=50
+
+
+{
+results: [
+{
+gender: "female",
+name: {
+title: "Ms",
+first: "Melodie",
+last: "Ambrose"
+},
+location: {
+street: {
+number: 7426,
+name: "St. Lawrence Ave"
+},
+city: "Delisle",
+state: "Newfoundland and Labrador",
+country: "Canada",
+postcode: "P7J 6X0",
+coordinates: {
+latitude: "-61.4955",
+longitude: "35.5573"
+},
+timezone: {
+offset: "+4:00",
+description: "Abu Dhabi, Muscat, Baku, Tbilisi"
+}
+},
+email: "melodie.ambrose@example.com",
+login: {
+uuid: "d0f9bd45-05f8-47a5-bc54-05588e4e4817",
+username: "crazybear412",
+password: "emma",
+salt: "t7HCNv7W",
+md5: "9a533802073655570e08bf315b82276b",
+sha1: "099678beb9cd54a246041badaeb56f8599dd984f",
+sha256: "a8968e95a6c1f29b0e7160ab43fe5dfd50bd1d15ade14384130581863e690aea"
+},
+dob: {
+date: "1961-12-01T13:49:01.984Z",
+age: 59
+},
+registered: {
+date: "2011-11-02T00:46:12.203Z",
+age: 9
+},
+phone: "928-251-4214",
+cell: "628-967-1411",
+id: {
+name: "",
+value: null
+},
+picture: {
+large: "https://randomuser.me/api/portraits/women/52.jpg",
+medium: "https://randomuser.me/api/portraits/med/women/52.jpg",
+thumbnail: "https://randomuser.me/api/portraits/thumb/women/52.jpg"
+},
+nat: "CA"
+}
+],
+info: {
+seed: "cc15827c1ed047f6",
+results: 1,
+page: 1,
+version: "1.3"
+}
+}
---
+++ app.js
@@ -0,0 +1,51 @@
+const btn = document.getElementById('btn');
+const results = document.getElementById('output');
+const img = document.querySelector('.loading-img');
+
+function setLoadingTrue() {
+  img.style.display = 'block';
+}
+
+function setLoadingFalse() {
+  img.style.display = 'none';
+}
+
+async function fetchUsers() {
+  setLoadingTrue();
+  const BASE_URL = `https://cors-anywhere.herokuapp.com/https://randomuser.me/api/?results=50`;
+  const res = await fetch(`${BASE_URL}`);
+  const json = await res.json();
+  console.log(json);
+  showDataDOM(json);
+}
+
+function showDataDOM(json) {
+  let output = '';
+  setLoadingFalse();
+  json.results.forEach((user) => {
+    output += `
+    <div class="wrapper"> 
+    <img src="${user.picture.medium}" class="card--img"/> 
+    <ul class="list">
+      <li class="collection-item">Gender: ${
+        user.gender ? user.gender : ''
+      } </li>
+      <li class="collection-item">name:${
+        user.name.first ? user.name.first : ''
+      } </li>
+      <li class="collection-item">location: ${
+        user.location.city ? user.location.city : ''
+      }</li>
+      <li class="collection-item">email: ${user.email ? user.email : ''}</li>
+      <li class="collection-item">UUID: ${
+        user.login.uuid ? user.login.uuid : ''
+      }</li>
+    </ul>
+    </div> 
+    `;
+  });
+  results.innerHTML = output;
+}
+
+// EVENT LISTENERS
+btn.addEventListener('click', fetchUsers);
